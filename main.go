6051a041f76f9589c5268b5f721fd74a80424c9d
k-um-i <rsilen@proton.me>
2025-04-28 22:03:24 +0300

    Implemented 'passwd' command for changing the password of an existing database.

--- main.go
+++ main.go
@@ -7,22 +7,24 @@ import (
 	"log"
 	"os"
 
-	"COMPSEC300_PM/encryption"
+	enc "COMPSEC300_PM/encryption"
 	"COMPSEC300_PM/gui"
-	"golang.org/x/term"
 	"syscall"
+
+	"golang.org/x/term"
 )
 
 // Main function that takes two commandline arguments.
-//  1. Command (open | create)
+//  1. Command (open | create | passwd)
 //     open is used for opening an existing password database
 //     create is used for creating an empty password database
+//     passwd is used for changing the password of an existing database
 //  2. filename
 //     specifies the filename of the database to be opened/created
 func main() {
 	// Check that a command argument is present
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: [open|create] <filename>")
+		fmt.Println("Usage: [open|create|passwd] <filename>")
 		os.Exit(1)
 	}
 
@@ -100,9 +102,8 @@ func main() {
 		if err != nil {
 			log.Fatal("Error reading password: ", err)
 		}
-		fmt.Println()
 
-		fmt.Print("Confirm password: ")
+		fmt.Print("\nConfirm password: ")
 		pass2, err := term.ReadPassword(int(syscall.Stdin))
 		if err != nil {
 			log.Fatal("Error reading password: ", err)
@@ -134,10 +135,80 @@ func main() {
 		gui.DbPass = password
 		gui.StartGui()
 
+	case "passwd":
+		passwdCmd := flag.NewFlagSet("passwd", flag.ExitOnError)
+		passwdCmd.Parse(os.Args[2:])
+
+		// Check that filename arg is present
+		if passwdCmd.NArg() < 1 {
+			fmt.Println("Usage: passwd <filename>")
+			os.Exit(1)
+		}
+		filename := passwdCmd.Arg(0)
+
+		// Check that the file exists
+		if _, err := os.Stat(filename); os.IsNotExist(err) {
+			fmt.Printf("Error: File \"%s\" does not exist.\n", filename)
+			os.Exit(1)
+		}
+
+		// Prompt user for old password first
+		fmt.Print("Enter password: ")
+		passwordBytes, err := term.ReadPassword(int(syscall.Stdin))
+		if err != nil {
+			log.Fatal("Error reading password: ", err)
+		}
+		fmt.Println()
+
+		oldpassword := string(passwordBytes)
+
+		// Read database file contents
+		fileContent, err := os.ReadFile(filename)
+		if err != nil {
+			log.Fatalf("Error reading file %s: %v\n", filename, err)
+		}
+
+		// Attempt to decrypt database contents
+		dbContents, err := enc.DecryptContents(fileContent, oldpassword)
+		if err != nil {
+			log.Fatalf("Error decrypting database: %v\n", err)
+		}
+
+		// Prompt for new password and confirm
+		fmt.Print("Enter new password: ")
+		pass1, err := term.ReadPassword(int(syscall.Stdin))
+		if err != nil {
+			log.Fatal("Error reading password: ", err)
+		}
+
+		fmt.Print("\nConfirm new password: ")
+		pass2, err := term.ReadPassword(int(syscall.Stdin))
+		if err != nil {
+			fmt.Println("Error reading password: ", err)
+			os.Exit(1)
+		}
+		fmt.Println()
+
+		// Check that provided passwords match
+		if string(pass1) != string(pass2) {
+			fmt.Println("Error: Passwords do not match.")
+			os.Exit(1)
+		}
+
+		newpassword := string(pass1)
+
+		// Decrypt the database using the new password
+		err = enc.UpdateDatabase(dbContents, newpassword, filename)
+		if err != nil {
+			log.Fatalf("Error re-encrypting database: %v\n", err)
+		}
+
+		fmt.Println("Successfully updated database password.")
+
 	default:
 		// Default case if user provided command is not recognized
 		fmt.Printf("Unknown command: %s\n", command)
-		fmt.Println("Usage: [open|create] <filename>")
+		fmt.Println("Usage: [open|create|passwd] <filename>")
 		os.Exit(1)
 	}
 }
